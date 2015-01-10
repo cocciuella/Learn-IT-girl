aee485ce9ce7c71ca48f0cca5750e080ca5a1a00
cocciuella <scierska.karolina@gmail.com>
2015-01-10 23:15:35 +0100

    Cleaning and preparing empty files

--- View/person_row.php
+++ View/person_row.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: karolin
- * Date: 2015-01-10
- * Time: 21:24
- */ 
--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
+include('./log_in.php');
+
 const DB_HOSTNAME = 'localhost';
 const DB_DATABASE = 'match_it';
-const DB_USERNAME = '';
-const DB_PASSWORD = '';
 
 const DEBUG_DISPLAY = true;
 const DEBUG_ERROR_LEVEL = E_ALL;
